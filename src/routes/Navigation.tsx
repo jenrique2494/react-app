35af6c81c8dc361716c3713e0eaa199c13b5c374
Jesus Enrique <jesusenrique2494@gmail.com>
2021-11-28 19:17:20 -0300

    formularios dinamicos listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,7 +6,8 @@ import {
   NavLink
 } from 'react-router-dom';
 import logo from '../logo.svg'
-import { RegisterPage, FormikBasicPage,FormikYupPage, FormikComponents, FormikAbstraction } from '../03-forms/pages';
+import { RegisterPage, FormikBasicPage,FormikYupPage, FormikComponents, FormikAbstraction, RegisterFormikPage, DynamicForm } from '../03-forms/pages';
+
 
 
 export const Navigation = ()=> {
@@ -20,6 +21,9 @@ export const Navigation = ()=> {
               <NavLink className={({ isActive }) => isActive ? 'nav-active' : '' }  to="/register" end>Register Page</NavLink>
             </li>
             <li>
+              <NavLink className={({ isActive }) => isActive ? 'nav-active' : '' }  to="/formik-register" end>Register Formik Page</NavLink>
+            </li>
+            <li>
               <NavLink className={({ isActive }) => isActive ? 'nav-active' : '' }  to="/formik-basic" end>Formik Basic</NavLink>
             </li>
             <li>
@@ -31,14 +35,19 @@ export const Navigation = ()=> {
             <li>
               <NavLink className={({ isActive }) => isActive ? 'nav-active' : '' }  to="/formik-abstraction" end>Formik Abstraction</NavLink>
             </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? 'nav-active' : '' }  to="/dynamic-form" end>Dynamic Form</NavLink>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/formik-register" element={<RegisterFormikPage/>}/>
           <Route path="/formik-basic" element={<FormikBasicPage />}/>
           <Route path="/formik-yup" element={<FormikYupPage/>}/>  
           <Route path="/formik-components" element={<FormikComponents/>}/>  
           <Route path="/formik-abstraction" element={<FormikAbstraction/>}/>  
+          <Route path="/dynamic-form" element={<DynamicForm/>}/>  
         </Routes>
       </div>
     </Router>
